a492d3a0627ee2630b8328216b819f750ce097c9
Pierre Moulonguet | MBP <pierre@pixelium.fr>
2016-07-17 17:27:43 +0200

    first commit

---
+++ azurway-sdk-test/SdkTestDebugViewController.h
@@ -0,0 +1,15 @@
+//
+//  EntryDebugViewController.h
+//  AW-PI-demo
+//
+//  Created by Timothée Bilodeau on 16/06/16.
+//  Copyright © 2016 Timothée Bilodeau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SdkTestDebugViewController : UIViewController
+
+@property (strong, nonatomic) UIViewController *containerVC;
+
+@end
